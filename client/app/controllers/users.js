9680f8a8c7e94fa237e7c443b136f76398c4aa45
Orion Supergan <orionsuperman@gmail.com>
2016-04-04 16:13:28 -0700

    fixed row choice options only being 1

--- client/app/controllers/users.js
+++ client/app/controllers/users.js
@@ -2,6 +2,12 @@ console.log('users.js');
 
 game_app.controller('UserController', function($scope, $location, UserFactory, CurrentFactory){
 
+	var gameTable = [[],
+			 [0, 30, -10, 10],
+			 [0, -10, 20, -20],
+			 [0, -10, 20, -30]
+			];
+
 	$scope.addUser = function(){
 		UserFactory.create($scope.new_user, function(data){
 			
